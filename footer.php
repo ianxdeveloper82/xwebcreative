19b16a9f9efdfa2da23bfea17f96c0576d7113e0
ianxdeveloper82 <159924138+ianxdeveloper82@users.noreply.github.com>
2024-12-16 08:37:21 +0000

    footer share btns

--- footer.php
+++ footer.php
@@ -8,11 +8,11 @@
                         <div class="source-org copyright"><?php bloginfo( 'name' ); ?>&copy;<?php echo date('Y'); ?> </div>
                         <div>0752 267 0454</div>
                     </div>  
-                    <!--
+                    
                     <div class="share">
-                   		 <?php // dynamic_sidebar( 'sidebar1' ); ?>
+                   		 <?php dynamic_sidebar( 'sidebar1' ); ?>
                     	<!--<div class="icon-share-alt"></div>-->
-                   <!-- </div> -->
+                   </div> <!-- -->
                     <div class="email">
                     <a href="mailto:ianxdeveloper82@gmail.com?Subject=Web%20Portfolio" target="_top">hello@xwebcreative.com</a>
                     </div>
